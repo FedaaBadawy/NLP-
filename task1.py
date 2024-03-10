9ed6fb9801c4a80e9263f8edcb9111557afb9ae5
FedaaBadawy <144146639+FedaaBadawy@users.noreply.github.com>
2024-03-10 15:24:33 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,28 @@
+# import nltk
+# nltk.download()
+
+from nltk import sent_tokenize , word_tokenize
+
+text = input("Enter a text: ")
+
+print("Choose a number:")
+print("1: Print tokenized words")
+print("2: Print tokenized sentences")
+print("3: Print output using split function")
+
+while True:
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        print(word_tokenize(text))
+
+    elif choice == '2':
+        print(sent_tokenize(text))
+
+    elif choice == '3':
+        print(text.split())
+
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.")
+
+# NLTK is a leading platform for building Python programs to work with human language data. NLTK is available for Windows, Mac OS X, and Linux. Best of all, NLTK is a free, open source, community-driven project.
